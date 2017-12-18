d16c0f550daf60e6c7ddc84e935fb6d4e8b74ab5
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2017-12-18 14:52:01 +0530

    Create variabledisplay.c

---
+++ Beginner/variabledisplay.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char name[25]="Sachin Ramesh Tendulkar",nickname[15]="Little Master";
+int runodi=18426,matchodi=463,i;
+float batodi=44.83;
+for(i=0;i<25;i++)
+{
+printf("%s \n",name[i]);
+}
+for(i=0;i<15;i++)
+{
+printf("%s \n",nickname[i]);
+}
+printf("%d \n",runodi);
+printf("%f \n",batodi);
+printf("%d \n",matchodi);
+}
