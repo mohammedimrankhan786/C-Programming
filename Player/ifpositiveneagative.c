ce0860f105b4c9f71f326aea973db749db26d1f3
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2017-12-19 12:08:30 +0530

    Create ifpositiveneagative.c

---
+++ Player/ifpositiveneagative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ( )
+{
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if (num<0)
+printf("The number is negative") ;
+else
+printf("The number is positive");
+return 0;
+}
