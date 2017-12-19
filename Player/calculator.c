46e0acb6a9f6c56bb5e7ad57a9d33dfcc94ad853
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2017-12-19 11:21:28 +0530

    Create calculator.c

---
+++ Player/calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a=0,b=0,add,sub,mul,div,mod;
+scanf("%d",&a);
+scanf("%d",&b);
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+printf("%d %d %d %d %d",add,sub,mul,div,mod);
+}
