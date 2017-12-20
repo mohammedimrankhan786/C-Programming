1aae50e904ed82ce112c35f1498c15b78b5265a0
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2017-12-20 11:58:46 -0800

    Create primerange.c

---
+++ Problems/primerange.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i, prime,upper,lower,n;
+clrscr();
+printf("\nEnter the limit:");
+scanf("\n %d \n %d", &lower,&upper);
+printf("\nPrime numbers are:");
+for(n=lower+1;n<upper;n++)
+{
+prime=1;
+for(i=2;i<n/2;i++)
+if(n%i==0)
+{
+prime=0;
+break;
+}
+if(prime)
+printf("\t%d", n);
+}
+getch();
+return 0;
+}
