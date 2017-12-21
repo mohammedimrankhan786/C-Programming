496800bee1732ef15cd12305e5420116a3c9db46
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2017-12-21 10:58:40 -0800

    Update lcm.c

--- Problems/lcm.c
+++ Problems/lcm.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 int main() 
 {
-int a, b, x, y, t, gcd, lcm;
+int a,b,x,y,t,gcd,lcm;
 clrscr(); 
 printf("\n Enter two numbers:");
 scanf("\n %d%d",&x,&y);
-a = x;
-b = y;
+a=x;
+b=y;
 while(b!=0) 
 {
-t = b;
-b = a % b;
-a = t;
+t=b;
+b=a%b;
+a=t;
 }
-gcd = a;
-lcm = (x*y)/gcd;
-printf("Least common multiple of %d and %d = %d\n", x, y, lcm);
+gcd=a;
+lcm=(x*y)/gcd;
+printf("LCM is %d\n",lcm);
+getch();
 return 0;
 }
