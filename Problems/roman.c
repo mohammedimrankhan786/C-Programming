1484527f46cccf0681e80c77eaeef46a96fab72b
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-01-01 04:11:05 -0800

    Update roman.c

--- Problems/roman.c
+++ Problems/roman.c
@@ -4,6 +4,7 @@ int main(void)
 {
 char r[10];
 int I,V,X,i,l,n[10],s=0;
+clrscr();
 printf("Enter the roman:\n");
 scanf("%s",r);
 l=strlen(r);
@@ -18,16 +19,20 @@ if(n[i]=='I')
 n[i]=1;
 else if(n[i]=='V')
 n[i]=5;
-else 
+else
 n[i]=10;
 }
 printf("\nInteger value is:");
 
 for(i=0;i<l;i++)
 {
+if(n[i]<n[i+1])
+s=n[i]-s;
+else
 s=s+n[i];
 }
 
 printf("\n %d",s);
+getch();
 return 0;
 }
