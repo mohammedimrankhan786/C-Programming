0834fc6281b2ac1914636b4f9919c5a7dcfaa7de
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-01-08 12:15:40 -0800

    Create structtypedef.c

---
+++ Pro/structtypedef.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+struct Rooms
+{
+int room_no
+char occupant_name[25]:
+int entry_date:
+int occupants:
+int concession:
+char booked:
+}
+void main()
+typedef struct Rooms Hotel;
+{
+Hotel h[30];
+int i;
+for(i=0;i<30;i++)
+{
+scanf("\n%d",&room_no);
+scanf("\n %s",occupant_name);
+scanf("\n %d",&entry_date);
+scanf("\n %d",&occupants);
+scanf("\n %d",&concession);
+scanf("\n %c",&booked);
+}
+for(i=0;i<30;i++)
+{
+if(Hotel.occupants==0)
+{
+printf("\n %d",room_no);
+}
+}
+}
