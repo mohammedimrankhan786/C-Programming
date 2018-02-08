e290c26459fcff90d3200ff7f44d151ef41e91c8
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-02-08 07:37:00 -0800

    create arraymedian.c

---
+++ Problems/arraymedian.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[20],t,j,median;
+    printf("\n Enter the number of elements:");
+    scanf("\n %d",&n);
+    printf("\n Enter the elements:");
+    for(i=0;i<n;i++)
+    {
+       scanf("\n %d",&a[i]); 
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+        if(a[j]<a[j+1])
+        t=a[j];
+        a[j]=a[j+1];
+        a[j+1]=t;
+        }
+    }
+    printf("\n Sorted array:");
+    for(j=0;j<n;j++)
+    {
+        printf("%d",a[j]);
+    }
+    for(j=0;j<n;i++)
+{
+	n=a[j];
+	median=(n+1)/2;
+}
+printf("%d",median);
+    return 0;
+}
