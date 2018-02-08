0e6d9bd298198a5be9287714c6dbd09d225bb63a
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-02-08 06:51:56 -0800

    update nsum.c

--- Problems/nsum.c
+++ Problems/nsum.c
@@ -2,11 +2,9 @@
 int main(void)
 {
 int n,sum=0;
-clrscr();
 printf("\n Enter the number to find its sum:");
 scanf("\n %d",&n);
 sum=(n*(n+1))/2;
 printf("\n The sum of %d is %d",n,sum);
-getch();
 return 0;
 }
